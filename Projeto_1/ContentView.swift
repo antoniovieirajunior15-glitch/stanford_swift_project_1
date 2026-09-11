d944bbc3e90c644b15e560ef74ee1822438d4a32
Antonio Gilvan <antoniovieirajunior15@gmail.com>
2026-09-11 14:49:52 -0300

    Initial Commit

---
+++ Projeto_1/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Projeto_1
+//
+//  Created by Antonio Gilvan Costa Vieira Junior on 11/09/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Projeto_1/Projeto_1App.swift
@@ -0,0 +1,17 @@
+//
+//  Projeto_1App.swift
+//  Projeto_1
+//
+//  Created by Antonio Gilvan Costa Vieira Junior on 11/09/26.
+//
+
+import SwiftUI
+
+@main
+struct Projeto_1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
